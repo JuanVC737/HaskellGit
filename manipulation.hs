d2ed14e2f512384b1e153dad15b0fdd66d14db08
Juan Corsino <CorsinJV20@pwcs-edu.org>
2019-05-23 15:15:06 -0400

    Branch 1 iteration

--- manipulation.hs
+++ manipulation.hs
@@ -1,3 +1,5 @@
 stringStuff n s = if mod n 2 == 0 then s else (reverse s)
 
-boustropheboi xs = 
+boustropheboi xs = [stringStuff n s | s <- xs, n <- [0..length xs]]
+
+splittinSkulls xs = take 20 xs
